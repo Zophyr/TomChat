87c336311291dc8e673548d4f89224d3c3dc516b
Zophyr <zhangjliang@mail2.sysu.edu.cn>
2020-06-26 22:15:35 +0800

    feat: response weather with more detail

--- src/tool/weather.js
+++ src/tool/weather.js
@@ -3,27 +3,33 @@ const request = require("request")
 // 请求参数解码
 const urlencode = require("urlencode")
 
+const fetch = require('node-fetch')
+
 // 配置文件
 const config = require("../../config/config")
 
 module.exports = (info) => {
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
         let city = info.replace(/\s+/g, "").replace(/[\n\r]/g, "").replace("天气", "") || "东莞"
-        let url = `https://free-api.heweather.net/s6/weather/now?location=${urlencode(city)}&key=${config.weatherKey}`
+        let baseUrl = `https://free-api.heweather.net/s6/weather/now?location=${urlencode(city)}&key=${config.weatherKey}`
+        let lifeUrl = `https://free-api.heweather.net/s6/weather/lifestyle?location=${urlencode(city)}&key=${config.weatherKey}`
 
-        request(url, (error, response, body) => {
-            if (!error && response.statusCode == 200) {
-                let res = JSON.parse(body).HeWeather6[0]
-                if (res.status === "ok") {
-                    resolve(city + "现在天气：" + res.now.tmp + "°C")
-                }
-                else {
-                    resolve(res.status)
-                }
-            }
-            else {
-                resolve("请求失败")
-            }
-        })
+        const responseBase = await fetch(baseUrl);
+        const resBase = await responseBase.json();
+
+        const responseLife = await fetch(lifeUrl);
+        const resLife = await responseLife.json();
+
+        resolve(showWeather(resBase.HeWeather6[0], resLife.HeWeather6[0]))
     })
+}
+
+function showWeather(resBase, resLife) {
+    let basic = "📍" + resBase.basic.location + "当前气温 " + resBase.now.tmp + "°C，天气" + resBase.now.cond_txt
+    let advice = "🍄体感：" + resLife.lifestyle.find(element => element.type == "comf").txt
+    let wear = "👘穿衣：" + resLife.lifestyle.find(element => element.type == "drsg").txt
+    let sport = "🏃‍运动：" + resLife.lifestyle.find(element => element.type == "sport").brf + "运动" + "，" + resLife.lifestyle.find(element => element.type == "sport").txt
+    let travel = "🎒外出：" + resLife.lifestyle.find(element => element.type == "trav").txt
+
+    return basic + '\n\n' + advice + '\n\n' + wear + '\n\n' + sport + '\n\n' + travel
 }
