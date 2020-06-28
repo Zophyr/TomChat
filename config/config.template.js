b0bc122ae88ceeeef7cc9a271fb56d3e2bc9118b
Zophyr <zhangjliang@mail2.sysu.edu.cn>
2020-06-28 16:34:18 +0800

    Update config.template.js

--- config/config.template.js
+++ config/config.template.js
@@ -1,7 +1,18 @@
 module.exports = {
     // 设定协议的token，本项目基于wechaty-puppet-padplus
-    token: "puppet_padplus_xxxx",
-    
+    token: "puppet_padplus_xxx",
+  
     // 机器人名字
-    name: "xxxxx"
-}
+    name: "xxx",
+  
+    weatherKey: "xxx",
+  
+    robotKey: "xxx",
+  
+    tomato: "xxx",
+  
+    //nasa: "xxx",
+  
+    avatardata_history: "xxx"
+  };
+  
